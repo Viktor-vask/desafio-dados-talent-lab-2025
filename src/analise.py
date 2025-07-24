41856bd93066a7cd31e4ef2b109935738e1831c0
Viktor-vask <viktor.vasconcello@gmail.com>
2025-07-24 17:18:10 -0400

    feat: Script para análise exploratória de dados (E.D.A)

---
+++ src/analise.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+# --- Documentação ---
+# Este script realiza a E.D.A
+# Carrega o conjunto de dados tratado e gera gráficos
+# os graficos são referentes a 
